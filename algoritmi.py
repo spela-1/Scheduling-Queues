8fa28d4dc3a7f65832837ded2e09048ec1eae189
Spela Bernardic <a.spela.bernardic@gmail.com>
2022-12-20 00:57:47 +0100

    SRPT

--- algoritmi.py
+++ algoritmi.py
@@ -76,32 +76,58 @@ def SRPT(opravila):
     cakanje = []
     vrsta = []
     t=opravila[0][1] #če se prvo opravilo začne po času 0
-    i=0 #indeks zadnjega opravila postavljenega v vrsto:
-    while 1:
+    i=0 #indeks naslednjega opravila ki bo postavljen v vrsto:
+    dokončana = 0
+    while dokončana < len(opravila):
 
         #vsa opravila, ki so prišla do časa t postavi v vrsto
-        while opravila[i][1]<t:
+        while opravila[i][1]<=t:
             vrsta.append(opravila[i])
-            i += 1
+            if i < len(opravila):
+                i += 1
+
+
         vrsta = sorted(vrsta, key=lambda item: item[2])  #razvrsti po length
         
         trenutno = vrsta[0]
 
-# t1 od začetka intervala 
+        if i < len(opravila):
+            naslednje = opravila[i]
+        else:
+            naslednje = (p, 100000000 , 0)       # smislimo si fiktivo opravilo tako, da bo {naslednje[1]< trenutno[2] + t} vedno ne res
 
 
-        if opravila[i+1][1]< trenutno[2] + t:   #če bo naslednje opravilo prej prišel preden se bo trenutno zaključilo
 
-            if opravila[i+1][2]<trenutno[2]:    #če je dolžina krajša od trenutnega
-                trenutno[2] = trenutno[2] - t1   #shranimo preostali čas ki ga trenutno opravilo potrebuje, da bo končano
+
+        if naslednje[1]< trenutno[2] + t:   #če bo naslednje opravilo prej prišel preden se bo trenutno zaključilo
+
+            if opravila[i][2]<trenutno[2]:    #če je dolžina krajša od trenutnega
+                trenutno[2] = trenutno[2]-(t-t1)  #shranimo preostali čas ki ga trenutno opravilo potrebuje, da bo končano t1 od začetka intervala 
                 vrsta.append(trenutno)          #in trenutno gre nazaj v vrsto
-                t = opravila[i+1][1]            #čas se spremeni
-                i += 1
+                t1 = t                          #shrani se čas kdaj se je začelo to opravilo izvajati
+                t = opravila[i+1][1] + 1           #čas se spremeni
+                
             else:
                 vrsta.append(opravila[i+1])
                 vrsta = sorted(vrsta, key=lambda item: item[2])
-                i += 1
+                
 
-         # SICER se trenutno opravilo se zaključi
+        else:                                       # SICER se trenutno opravilo zaključi
+            dokončana += 1
+            cas_cakanja = t - trenutno[1]
+            cakanje.append(cas_cakanja)
+            t = t + trenutno[2]
+            vrsta.remove(trenutno)
+
+
+    return cakanje
             
-#NEVEMMMM
+
+opravila_SRPT = [('p1', 0, 1), ('p2', 0, 2), ('p3', 2, 4), ('p4', 3, 5), ('p5', 7, 1)]
+
+cakanje_SRPT = SRPT(opravila_SRPT)     
+
+
+#popraviti je treba:
+# * v 84 vrstici narediti tako, da ko je i > len(opravila) potem se naj zanka ne izvaja več
+# * preveriti je treba če i-ji sicer štimajo in če t štima
