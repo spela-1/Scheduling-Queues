456f337e16aae57277fa2eaefee70befd91d5160
Ioann Stanković <70526098+Posrek@users.noreply.github.com>
2022-12-29 02:09:41 +0100

    Add files via upload
    CAS PRIHODA - BETA PORAZDELITEV in STEVILO PRIHODOV - NORMALNA PORAZDELITEV

---
+++ Generiranje_prihodov.R
@@ -0,0 +1,36 @@
+# Generiranje podatkov
+library(dplyr)
+
+
+
+# Rabimo upostevati pogoj Var < E(1 - E) ! 
+
+#Komentar: prof je omenil da lahko dolocimo min in max beta porazdelitve, to lahko naredimo pri enakomerno zvezni porazdelitvi, pri beta te opcije ni
+
+#============================================================================ CAS PRIHODA - BETA PORAZDELITEV in STEVILO PRIHODOV - NORMALNA PORAZDELITEV  ==================================================================================
+casi_prihodov = function(E, Var, n, velikost) {
+  alpha = E*(E*(1-E)/Var - 1)
+  beta = (1 - E)*(E*(1-E)/Var - 1)
+  b = rbeta(n, alpha, beta)*velikost
+  b = round(b)
+  p = abs(rnorm(n,0, 0.5))*n
+  p = round(p)
+  a = bind_cols(p,b)
+  colnames(a) = c("cas_prihoda", "dolzina_opravila")
+  a <- a %>% arrange((cas_prihoda))
+  return(a)
+}
+#==========================================================================================================================================================================
+
+#Komentar: upanje normalne je 0, in varianca je 0.5, doloceno po obcutku, potrebno dodatnega premisleka in komentarja
+
+# druga opcija je da generiram nakljucne vrednosti na intervalu z enakomerno zvezno porazdelitvijo, mogoce je to tudi profesor mislil s tem da lahko tam dolocamo min/max
+
+a = casi_prihodov(0.5,0.1,100,10)
+
+summary(a)
+
+
+#runif(n, min = 0, max = velikost), problem te je da se nabira na sredini
+
+# generira tudi dolzine prihodov 0
