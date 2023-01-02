1753bde6655eabbc68b027b6b72e4cbd165eeac7
Spela Bernardic <a.spela.bernardic@gmail.com>
2023-01-02 15:24:22 +0100

    popravki +  test

--- algoritmi.py
+++ algoritmi.py
@@ -28,12 +28,14 @@ cakanje1 = FCFS(opravila1)
 
 #Shortest job first
 def SJF(opravila):
-# vzame množico opravil in izračuna čas čakanja
+# vzame množico opravil in izračuna povprečen čas čakanja
 # opravila je list opravil, opravilo = (id, arrival, length)
     opravila = sorted(opravila, key=lambda item: item[1])  #razvrsti po arrival
     cakanje = []
     vrsta = []
     t=opravila[0][1] #če se prvo opravilo začne po času 0
+    n = len(opravila)
+
     while len(opravila) > 0:
         opravilo = opravila[0]
         opravila.remove(opravilo)   
@@ -48,7 +50,8 @@ def SJF(opravila):
             while opravila[0][1]<= t :
                 vrsta.append(opravila[0])
                 opravila.remove(opravila[0])
-            vrsta = sorted(vrsta, key=lambda item: item[2])  #razvrsti po length
+
+        vrsta = sorted(vrsta, key=lambda item: item[2])  #razvrsti po length
         
         #izvaja opravila v vrsti dokler ni t enak času prihoda naslednjega opravila ali pa je list opravil prazen
         if len(opravila) !=0:
@@ -66,15 +69,13 @@ def SJF(opravila):
                 cakanje.append(cas_cakanja)
                 t = opravilo1[2] + t  #čas po končanem opravilu
 
-    return cakanje
+    return sum(cakanje)/n
 
+# zadeva zdej dela ampak definitivno ni najbolj učinkovita. mrbit bi blo boljš delat z indeksi...?
 opravila_SJF = [('p1', 0, 1), ('p2', 0, 2), ('p3', 2, 4), ('p4', 3, 5), ('p5', 7, 1)]
 
 cakanje_SJF = SJF(opravila_SJF)
-
-
-# zadeva zdej dela ampak definitivno ni najbolj učinkovita. mrbit bi blo boljš delat z indeksi...?
-
+print("SJF", cakanje_SJF)
 
 
 
@@ -90,6 +91,7 @@ def SRPT(opravila):
     t1 = opravila[0][1] #
     i=0 #indeks naslednjega opravila ki bo postavljen v vrsto:
     dokončana = 0
+    n = len(opravila)
 
     while opravila[i][1]<=t:
         vrsta.append(opravila[i])
@@ -141,29 +143,18 @@ def SRPT(opravila):
             vrsta.remove(trenutno)
 
 
-    return cakanje
+    return sum(cakanje)/n
             
 
-#opravila_SRPT1 = [['p1', 0, 1], ['p2', 0, 2], ['p3', 2, 4], ['p4', 3, 5], ['p5', 7, 1]]
-#opravila_SRPT2 = [['p1', 0, 5], ['p2', 1, 1], ['p3', 2, 4]]
-opravila_SRPT3 = [["1", 2, 6, 6], ["2", 5, 2, 2], ["3", 1, 8, 8], ["4", 0,3, 3], ["5", 4, 4, 4]]
-#opravila_SRPT4 = [["1", 0, 8],["2", 1, 4], ["3", 2, 2], ["4", 3, 1],["5", 4, 3], ["5", 5, 2]]
-opravila_SRPT5 = [["1", 0, 7,7], ["2", 1, 3, 3],["3", 3, 4, 4]]
+opravila_SRPT1 = [["1", 2, 6, 6], ["2", 5, 2, 2], ["3", 1, 8, 8], ["4", 0,3, 3], ["5", 4, 4, 4]]
+opravila_SRPT2 = [["1", 0, 7,7], ["2", 1, 3, 3],["3", 3, 4, 4]]
 
+cakanje_SRPT1 = SRPT(opravila_SRPT1)  
+cakanje_SRPT2 = SRPT(opravila_SRPT2)    
 
+print("SPRT1", cakanje_SRPT1)
+print("SPRT2", cakanje_SRPT2) #pravilen odgovor 8/3 
 
-#cakanje_SRPT1 = SRPT(opravila_SRPT1)  
-#cakanje_SRPT2 = SRPT(opravila_SRPT2)     
-cakanje_SRPT3 = SRPT(opravila_SRPT3)  
-#cakanje_SRPT4 = SRPT(opravila_SRPT4)
-cakanje_SRPT5 = SRPT(opravila_SRPT5) #pravilen odgovor 8
-
-print("srpt", cakanje_SRPT3)
-print(sum(cakanje_SRPT3))
-
-
-print("srpt5", cakanje_SRPT5)
-print(sum(cakanje_SRPT5))
 
 
 
@@ -178,6 +169,7 @@ def PSJF(opravila):
     t1 = opravila[0][1] #
     i=0 #indeks naslednjega opravila ki bo postavljen v vrsto:
     dokončana = 0
+    n =len(opravila)
 
     while opravila[i][1]<=t:
         vrsta.append(opravila[i])
@@ -229,14 +221,17 @@ def PSJF(opravila):
             vrsta.remove(trenutno)
 
 
-    return sum(cakanje)
+    return sum(cakanje)/n
 
 
-opravila_PSJF = [['p1', 0, 1, 1], ['p2', 0, 2, 2], ['p3', 2, 4, 4], ['p4', 3, 5, 5] , ['p5', 7, 1, 1]]
+opravila_PSJF1 = [['p1', 0, 1, 1], ['p2', 0, 2, 2], ['p3', 2, 4, 4], ['p4', 3, 5, 5] , ['p5', 7, 1, 1]]
 opravila_PSJF2 = [["1", 2, 6, 6], ["2", 5, 2, 2], ["3", 1, 8, 8], ["4",0, 3, 3], ["5", 4, 4, 4]]
 
-cakanje_PSJF = PSJF(opravila_PSJF)  
+cakanje_PSJF1 = PSJF(opravila_PSJF1)  
 cakanje_PSJF2 = PSJF(opravila_PSJF2)
+
+print("PSJF1", cakanje_PSJF1)
+print("PSJF2", cakanje_PSJF2)
 
 
 
@@ -250,6 +245,8 @@ def SPJF(opravila):
     cakanje = []
     vrsta = []
     t=opravila[0][1] #če se prvo opravilo začne po času 0
+    n= len(opravila)
+    
     while len(opravila) > 0:
         opravilo = opravila[0]
         opravila.remove(opravilo)   
@@ -264,7 +261,8 @@ def SPJF(opravila):
             while opravila[0][1]<= t :
                 vrsta.append(opravila[0])
                 opravila.remove(opravila[0])
-            vrsta = sorted(vrsta, key=lambda item: item[3])  #razvrsti po predicted length
+
+        vrsta = sorted(vrsta, key=lambda item: item[3])  #razvrsti po predicted length
         
         #izvaja opravila v vrsti dokler ni t enak času prihoda naslednjega opravila ali pa je list opravil prazen
         if len(opravila) !=0:
@@ -282,7 +280,16 @@ def SPJF(opravila):
                 cakanje.append(cas_cakanja)
                 t = opravilo1[2] + t  #čas po končanem opravilu
 
-    return cakanje
+    return sum(cakanje)/n
+
+
+opravila_SPJF = [('p1', 0, 1, 1), ('p2', 0, 2, 2), ('p3', 2, 4, 4), ('p4', 3, 5, 5), ('p5', 7, 1, 1)]
+cakanje_SPJF = SPJF(opravila_SPJF)
+print("SPJF", cakanje_SPJF)
+
+opravila_SPJF1 = [('p1', 0, 1, 2), ('p2', 0, 2, 1), ('p3', 2, 4, 4), ('p4', 3, 5, 3), ('p5', 7, 1, 2)]
+cakanje_SPJF1 = SPJF(opravila_SPJF1)
+print("SPJF1", cakanje_SPJF1)
 
 
 
@@ -297,6 +304,7 @@ def SPRPT(opravila):
     t1 = opravila[0][1] #
     i=0 #indeks naslednjega opravila ki bo postavljen v vrsto:
     dokončana = 0
+    n= len(opravila)
 
     while opravila[i][1]<=t:
         vrsta.append(opravila[i])
@@ -313,7 +321,7 @@ def SPRPT(opravila):
         if i < len(opravila):
             naslednje = opravila[i]
         else:
-            naslednje = ["p", 100000000 , 0, 0]       # izmislimo si fiktivo opravilo tako, da bo {naslednje[1]< trenutno[3] + t} vedno FALSE
+            naslednje = ["p", 100000000 , 0, 0, 0]       # izmislimo si fiktivo opravilo tako, da bo {naslednje[1]< trenutno[3] + t} vedno FALSE
 
 
 
@@ -350,7 +358,21 @@ def SPRPT(opravila):
             vrsta.remove(trenutno)
 
 
-    return cakanje
+    return sum(cakanje)/n
+
+
+
+opravila_SPRPT1 = [["1", 2, 6, 6, 6], ["2", 5, 2, 2, 2], ["3", 1, 8, 8, 8], ["4", 0, 3, 3, 3], ["5", 4, 4, 4, 4]]
+opravila_SPRPT2 = [["1", 0, 7, 7, 7], ["2", 1, 3, 3, 3],["3", 3, 4, 4, 4]]
+opravila_SPRPT3 = [["1", 0, 7, 7, 1], ["2", 1, 3, 3, 10],["3", 3, 4, 4, 4]]
+
+cakanje_SPRPT1 = SPRPT(opravila_SPRPT1)  
+cakanje_SPRPT2 = SPRPT(opravila_SPRPT2)
+cakanje_SPRPT3 = SPRPT(opravila_SPRPT3)    
+
+print("SPRPT1", cakanje_SPRPT1)
+print("SPRPT2", cakanje_SPRPT2) #pravilen odgovor 8/3 
+print("SPRPT3", cakanje_SPRPT3)
 
 
 #Preemptive Shortest Predicted Job First
@@ -364,6 +386,7 @@ def PSPJF(opravila):
     t1 = opravila[0][1] #
     i=0 #indeks naslednjega opravila ki bo postavljen v vrsto:
     dokončana = 0
+    n = len(opravila)
 
     while opravila[i][1]<=t:
         vrsta.append(opravila[i])
@@ -380,7 +403,7 @@ def PSPJF(opravila):
         if i < len(opravila):
             naslednje = opravila[i]
         else:
-            naslednje = ["p", 100000000 , 0, 0]       # izmislimo si fiktivo opravilo tako, da bo {naslednje[1]< trenutno[2] + t} vedno ne res
+            naslednje = ["p", 100000000 , 0, 0, 0]       # izmislimo si fiktivo opravilo tako, da bo {naslednje[1]< trenutno[2] + t} vedno ne res
 
 
 
@@ -415,4 +438,16 @@ def PSPJF(opravila):
             vrsta.remove(trenutno)
 
 
-    return sum(cakanje)
+    return sum(cakanje)/n
+
+opravila_PSPJF1 = [['p1', 0, 1, 1, 1], ['p2', 0, 2, 2, 2], ['p3', 2, 4, 4, 4], ['p4', 3, 5, 5, 5] , ['p5', 7, 1, 1, 1]]
+opravila_PSPJF2 = [["1", 2, 6, 6, 6], ["2", 5, 2, 2, 2], ["3", 1, 8, 8, 8], ["4",0, 3, 3, 3], ["5", 4, 4, 4, 4]]
+opravila_PSPJF3 = [["1", 2, 6, 6, 10], ["2", 5, 2, 2, 1], ["3", 1, 8, 8, 2], ["4",0, 3, 3, 7], ["5", 4, 4, 4, 4]]
+
+cakanje_PSPJF1 = PSPJF(opravila_PSPJF1)  
+cakanje_PSPJF2 = PSPJF(opravila_PSPJF2)
+cakanje_PSPJF3 = PSPJF(opravila_PSPJF3)
+
+print("PSPJF1", cakanje_PSPJF1)
+print("PSPJF2", cakanje_PSPJF2)
+print("PSPJF3", cakanje_PSPJF3)
