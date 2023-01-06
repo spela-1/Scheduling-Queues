f2294dd9e6460305999755d2f76fbb8bcc80a69f
Ioann Stanković <70526098+Posrek@users.noreply.github.com>
2023-01-06 13:15:53 +0100

    Add files via upload

--- Generiranje_prihodov_po_beta_porazdelitvi.R
+++ Generiranje_prihodov_po_beta_porazdelitvi.R
@@ -6,43 +6,37 @@ library(dplyr)
 # rabim izracunat dejansko varianco in dejansko varianco "shuma"
 
 #============================================================================ CAS PRIHODA - BETA PORAZDELITEV in STEVILO PRIHODOV - NORMALNA PORAZDELITEV  ==================================================================================
-casi_prihodov_beta = function(E, Var, n, velikost, shum) {
+casi_prihodov_beta = function(E, Var, n, velikost, shum, rt) {
   alpha = E*(E*(1-E)/Var - 1)
   beta = (1 - E)*(E*(1-E)/Var - 1)
-  dolzina_opravila = rbeta(n, alpha, beta)*velikost   # round v eno vrstico
-  dolzina_opravila = round(dolzina_opravila)
-  cas_prihoda = abs(rnorm(n,0, 0.5))*n
-  cas_prihoda = round(cas_prihoda)      # tuki dodajam se sum
+  dolzina_opravila = rbeta(n, alpha, beta)*velikost
   
-  alpha1 = E*(E*(1-E)/shum - 1)
-  beta1 = (1 - E)*(E*(1-E)/shum - 1)
-  noise = rbeta(n, alpha1, beta1)*velikost - E*velikost
-  noise = round(noise)
+  cas_prihoda = rexp(n, rt)     # tuki dodajam se sum
+  
+  noise = rnorm( n, 0, shum)
   dolzina_opravila_noise = noise + dolzina_opravila
-  a = cbind(cas_prihoda,dolzina_opravila,dolzina_opravila, dolzina_opravila_noise)
-  a = pmax(a,0)
+  a = cbind(cas_prihoda , dolzina_opravila, dolzina_opravila_noise)
+  a = pmax(a,10^-7)
   a = a[order(a[,1]),]
   return(a)
 }
 #==========================================================================================================================================================================
 #OPOMBA: dejanska pricakovana vrednost je E*velikost (varianca tudi ni pravilna, sam varianca ni linearna)
 
-
-# PARAMETRI:
+#KOMENTAR: as_tibble zna biti popolnoma nepotreben (in vse nadalje) , ce hocs to banano uredit po velikosti uporabs a = a[order(a[,1]),]
+# funkcijski parametri:
 # E = upanje/velikost
 # Var = varianca....nelinearna
 # n = stevilo prihodov
 # velikost = zgornja meja "burst time"
 
 
-#Komentar: upanje normalne je 0, in varianca je 0.5, doloceno po obcutku, potrebno dodatnega premisleka in komentarja
 
-
-#Komentar: generira tudi dolzine prihodov 0, ta problem lahko odpravimo preprosto da dodamo +1, (lahko se pojavi sum dolzine 0)
+#Komentar: Burst time je najmanj 10^-7
 #Komentar: moti me da sum lahko poveca najin maksimalen burst time
 #==========================================================================================================================================================================
 
-b = casi_prihodov_beta(0.5,0.1,10,10, 0.01)
+b = casi_prihodov_beta(0.5,0.1,10,10, 0.01,0.25)
 
 #========================================================================================================= raje bi s tem delal ker se mi zdi bolj pregledno
 mylist <- list(a=0.5,b=0.5,c=0.5) # neznam ta seznam razsirit
@@ -56,7 +50,7 @@ a = lapply(mylist,casi_prihodov,Var=0.1, n= 10, velikost = 10, shum = 0.01) # vr
 generiranje = function(k) {
   i = 1
   while (i < k) {
-  a = casi_prihodov_beta(0.5,0.1,10,10, 0.01)
+  a = casi_prihodov_beta(0.5,0.1,10,10, 0.01,0.25)
   b = rbind(b, a)
   i = i +1
   }
--- Generiranje_prihodov_po_normalni_porazdelitvi.R
+++ Generiranje_prihodov_po_normalni_porazdelitvi.R
@@ -3,30 +3,29 @@ library(dplyr)
 
 
 #============================================================================ CAS PRIHODA - NORMALNA PORAZDELITEV in STEVILO PRIHODOV - NORMALNA PORAZDELITEV  ==================================================================================
-casi_prihodov_normalne = function(E, Var, n, shum) {
-  dolzina_opravila = round(abs(rnorm(n,E, Var)))
+casi_prihodov_normalne = function(E, Var, n, shum, rt) {
+  dolzina_opravila = abs(rnorm(n,E, Var))
   
-  cas_prihoda = round(abs(rnorm(n,0, 0.5))*n)  # tuki dam absolutno vrednost, je mar to dovoljeno?, potem dobimo nekaj drugega vendarle
-       
-  
-  noise = round(rnorm(n,E, shum) - E) # lahko tudi pogledamo koliko je znasal posamezen sum
+  cas_prihoda = rexp(n, rt)  # tuki dam absolutno vrednost, je mar to dovoljeno?, potem dobimo nekaj drugega vendarle
+
+  noise = rnorm(n,0, shum) # lahko tudi pogledamo koliko je znasal posamezen sum
   
   dolzina_opravila_noise = noise + dolzina_opravila
   a = cbind(cas_prihoda,dolzina_opravila,dolzina_opravila, dolzina_opravila_noise)
-  a = pmax(a,0)
+  a = pmax(a,10^-7)
   a = a[order(a[,1]),]
   return(a)
 }
 #==========================================================================================================================================================================
 
-b = casi_prihodov_normalne(5,5,10, 1)
+b = casi_prihodov_normalne(5,5,10, 1, 0.25)
 
 # to vrze matriko teh podatkov k jo rabis rezat
 #=============================================================================== GENERIRANJE ===============================================================================
 generiranje2 = function(k) {
   i = 1
   while (i < k) {
-    a = casi_prihodov_normalne(5,5,10, 1)
+    a = casi_prihodov_normalne(5,5,10, 1, 0.25)
     b = rbind(b, a)
     i = i +1
   }
