0b5d0492feede9c1af856630932d527c5c3ce051
Ioann Stanković <70526098+Posrek@users.noreply.github.com>
2023-01-09 22:33:02 +0100

    Add files via upload

--- Vizualizacija_normalna.R
+++ Vizualizacija_normalna.R
@@ -23,7 +23,7 @@ Normalna = Normalna_porazdelitev_n[,0:4]
 
 x = c(4, 8, 12 , 16 , 20 , 24)
 
-z = 11.63321 # povprecni cas cakanja
+z = 3.559897 # povprecni cas cakanja
 
 df = cbind(x ,Normalna)
 
@@ -34,7 +34,7 @@ Tabela = df %>% pivot_longer( !x, names_to = "Algoritmi", values_to = "Povprecno
 #=========================================================================================== RISANJE POVP. CASA CAKANJA ==================PREVERI ALPHA IN BETA NUJNO===========================
 cakanje_norm = ggplot(Tabela, aes(x=x, y = `Povprecno cakanje`, col = Algoritmi)) +
   geom_line()+
-  geom_point() + labs(title = "Povprečni čas čakanja opravil za |N(5,5)|", subtitle = "E = 11.63321, Var = 9.237509 " ,y = "Povprečno čakanje (k=100)", x = "Število prihodov") +
+  geom_point() + labs(title = "Povprečni čas čakanja opravil za |N(5,5)|", subtitle = "E = 3.559897, Var = 17.32713 " ,y = "Povprečno čakanje (k=100)", x = "Število prihodov") +
   ylim(0, 1010) +
   scale_colour_manual(values=c('greenyellow','lightpink1',"tomato3", "lightskyblue"))
 #===================================================================================================================================================================
@@ -101,9 +101,9 @@ prihodi_norm_n4 = ggplot(n_4, aes(x=cas_prihoda, y=dolzina_opravila)) +
                    y=0, 
                    yend=dolzina_opravila), colour = "yellowgreen") +
   labs(y = "Dolžina opravila", x = "Čas prihoda") +
-  geom_line(aes(y=z), colour="grey",linetype="dashed") +
+  geom_line(aes(y=3.559897), colour="grey",linetype="dashed") +
   labs(title="Prihodi opravil (burst time)", 
-       subtitle="E = 11.63321, Var = 9.237509, n = 4, lambda = 0.25") +
+       subtitle="E = 3.559897, Var = 17.32713, n = 4, lambda = 0.25") +
   xlim(0, 12) + ylim(0,19)
 #=========================================================================
 
